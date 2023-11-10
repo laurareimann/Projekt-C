ea1f2337bd363bb87f07377a0b92deba4a5862d8
Gandeon <leon-mohr@hotmail.com>
2023-11-10 03:24:32 +0100

    Worked some more on it

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>
+        15-Minuten Stadt für Project C
+      </header>
+    </div>
+  )
+}
+
+export default App
