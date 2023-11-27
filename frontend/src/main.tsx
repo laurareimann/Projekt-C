3f9586113dc46b24beb117cdffc8bc71e9b014d7
Gandeon <leon-mohr@hotmail.com>
2023-11-27 04:04:12 +0100

    node module test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import App from './App.tsx'
 import './index.css'
 
