37a5f5cc6c346faebc49a675e32743063cac15cc
Gandeon <leon-mohr@hotmail.com>
2023-12-05 17:55:41 +0100

    added some more components and pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 import './App.css'
 
