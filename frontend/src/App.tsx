e22632c3473cfcf239af1903d753bab675affcfd
Laura R <la.reimann.lr@gmail.com>
2024-02-27 08:59:53 +0100

    declaration of global styles (DONE) Please enter the commit message for your changes. Lines starting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,127 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
 import { useState } from 'react'
 import styled from 'styled-components';
-
 import './App.css'
-//import GlobalStyle from './globalStyles.js';
+import {createGlobalStyle} from "styled-components";
 
-const GlobalStyle = styled.div`
+interface MyComponentProps {
+  color: string;
+  children: React.ReactNode;
+}
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+
+    // color palette
+    // double dashes are a naming convention often used to create reusable and easily identifiable custom properties for styling purposes in web development
+    --color--blue-1: #C5CAFC;
+    --color--blue-2: #8B96F9;
+    --color--blue-3: #3D50F5;
+    --color--blue-4: #0B1FD5;
+    --color--blue-5: #050E61;
+
+    --color--green-1: #9AD5CE;
+    --color--green-2: #48ADA1;
+    --color--green-3: #30736B;
+    --color--green-4: #1E4843;
+    --color--green-5: #122B28;
+    
+    --color--pink-1: #FFC2EA;
+    --color--pink-2: #FF47C2;
+    --color--pink-3: #B8007A;
+    --color--pink-4: #660044;
+    --color--pink-5: #3D0029;
+
+    // additional colors
+    --color--error-red-light: #FFF0F1;
+    --color--error-red-mid: #FA8686;
+    --color--error-red: #EE1D36;
+    --color--success-green-light: #EFFDF6;
+    --color--success-green-mid: #7FF0B8;
+    --color--success-green: #17C970;
+    --color--white-shade: #ffffff;
+    --color--black-shade: #000000;
+  }
+
   body {
+    background-color: var(--color--white-shade);
     margin: 0;
     padding: 20px;
-    background: grey;
     font-family: 'Roboto', sans-serif;
   }
 
   // typography
   h1 {
-  color: var(--color--blue-1);
-  line-height: 1.2;
-  margin-bottom: 0.5em;
-  font-weight: bold;
+  color: var(--color--blue-5);
+  line-height: 3.5rem;
+  font-size: 3rem;
+  font-weight: 600;
 }
-`
+  h2 {
+  color: var(--color--blue-5);
+  line-height: 2.5rem;
+  font-size: 2rem;
+  font-weight: 600;
+}
+  h3 {
+  color: var(--color--blue-5);
+  line-height: 2rem;
+  font-size: 1.25rem;
+  font-weight: 500;
+}
+  p {
+  color: var(--color--black-shade);
+  line-height: 1.5rem;
+  font-size: 1.25rem;
+  font-weight: 400;
+}
+`;
 
 
+const ColoredString = styled.p<{ color: string }>`
+  color: ${(props) => props.color};
+`;
+
+const ColoredParagrpah: React.FC<MyComponentProps> = ({ color, children }) => {
+  return <ColoredString color={color}>{children}</ColoredString>;
+};
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <GlobalStyle>
-      <div>
-        <header>
-          15-Minuten Stadt für Project C
-        </header>
-        <h1>h1</h1>
-      </div>
-    </GlobalStyle>
+    <div>
+      <GlobalStyle />
+      <h1>
+        15-Minuten Stadt für Project C
+      </h1>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <ColoredParagrpah color="var(--color--blue-1)">blue-1</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--blue-2)">blue-2</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--blue-3)">blue-3</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--blue-4)">blue-4</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--blue-5)">blue-5</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--green-1)">green-1</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--green-2)">green-2</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--green-3)">green-3</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--green-4)">green-4</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--green-5)">green-5</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--pink-1)">pink-1</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--pink-2)">pink-2</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--pink-3)">pink-3</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--pink-4)">pink-4</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--pink-5)">pink-5</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--error-red-light)">error-red-light</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--error-red-mid)">error-red-mid</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--error-red)">error-red</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--success-green-light)">success-green-light</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--success-green-mid)">success-green-mid</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--success-green)">success-green</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--white-shade)">white-shade</ColoredParagrpah>
+      <ColoredParagrpah color="var(--color--black-shade)">black-shade</ColoredParagrpah>
+    </div>
   )
 }
 
