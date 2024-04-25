a930a28001fd180a3a963a43127f6828e3756915
Laura R <la.reimann.lr@gmail.com>
2024-04-25 15:33:45 +0200

    routing: add content for homepage and about us plus some styling WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,12 +9,18 @@ import Footer from './components/Footer';
 import Button from './components/Buttons';
 import GlassButton from './components/GlassButtons';
 import BlurButton from './components/BlurButtons';
-
 import Location from './components/Location';
 import Result from './components/Result';
 import Input from './components/Inputforms';
 import ScoreContainer from './components/ScoreContainer';
 import QuizContainer from './components/QuizContainer';
+
+
+//pages
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/MainPage";
+import AboutUs from "./pages/AboutUs";
 
 
 
@@ -70,6 +76,7 @@ export const GlobalStyle = createGlobalStyle`
   line-height: 3.5rem;
   font-size: 3rem;
   font-weight: 600;
+  text-align: center;
 }
 h2 {
   color: var(--color--blue-5);
@@ -81,12 +88,12 @@ h2 {
 h3 {
   color: var(--color--blue-5);
   line-height: 2rem;
-  font-size: 1.25rem;
-  font-weight: 500;
+  font-size: 1.5rem;
+  font-weight: 600;
 }
 p {
   line-height: 1.5rem;
-  font-size: 1.25rem;
+  font-size: 1.125rem;
   font-weight: 400;
   margin:0;
 }
@@ -117,9 +124,12 @@ const ColoredParagrpah: React.FC<MyComponentProps> = ({ color, children }) => {
 
 // Weiß nicht ob der Container wirklich nötig ist aber für Ordnung ganz gut
 // Mein Footer wollte bloß nicht so wie ich wollte am Anfang
-const MainContainer = styled.main`
+const ContentContainer = styled.main`
   padding: 40px 0;
   margin: 0;
+  max-width: 1040px;
+  text-align: start;
+  
 `;
 
 
@@ -143,81 +153,25 @@ display: grid;
 grid-gap: 12px;
 `
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      <Header/>
-     
-      <MainContainer>
-        <GlobalStyle />  
-        <InputGrid>
-        <Result color='pink'>HAW Finkenau</Result>
-        <Location color='pink'>HAW Finkenau</Location>
-          <Input disabled={true}></Input>
-          <Input></Input>
-          <Input isValid={false}></Input>
-        </InputGrid>
-        <ButtonGrid>
-          <ContainerGrid>
-            <ScoreContainer color='blue'></ScoreContainer>
-            <ScoreContainer color='green'></ScoreContainer>
-            <ScoreContainer color='pink'></ScoreContainer>
-          </ContainerGrid>
-          <ContainerGrid>
-            <QuizContainer color='blue'></QuizContainer>
-            <QuizContainer color='green'></QuizContainer>
-            <QuizContainer color='pink'></QuizContainer>
-          </ContainerGrid>
-        </ButtonGrid>
-        <ButtonGrid>
-          <Button color='blue' onClick={handleClick}>NORMAL blue</Button>
-          <Button color='green' onClick={handleClick}>NORMAL green</Button>
-          <Button color='pink' onClick={handleClick}>NORMAL pink</Button>
-          <Button color='blue' disabled={true}>NORMAL blue disabled</Button>
-          <Button color='green' disabled={true}>NORMAL green disabled</Button>
-          <Button color='pink' disabled={true}>NORMAL pink disabled</Button>
-          <GlassButton color='blue' onClick={handleClick}>Glass Button blue</GlassButton>
-          <GlassButton color='green' onClick={handleClick}>Glass Button green</GlassButton>
-          <GlassButton color='pink' onClick={handleClick}>Glass Button pink</GlassButton>
-          <BlurButton color='blue' onClick={handleClick}>Blur Button Blue</BlurButton>
-          <BlurButton color='green' onClick={handleClick}>Blur Button Green</BlurButton>
-          <BlurButton color='pink' onClick={handleClick}>Blur Button Pink</BlurButton>
-        </ButtonGrid>
-        <h1>
-          15-Minuten Stadt für Project C
-        </h1>
-        <h1>h1</h1>
-        <h2>h2</h2>
-        <h3>h3</h3>
-        <ColoredParagrpah color="var(--color--blue-1)">blue-1</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--blue-2)">blue-2</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--blue-3)">blue-3</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--blue-4)">blue-4</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--blue-5)">blue-5</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--green-1)">green-1</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--green-2)">green-2</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--green-3)">green-3</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--green-4)">green-4</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--green-5)">green-5</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--pink-1)">pink-1</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--pink-2)">pink-2</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--pink-3)">pink-3</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--pink-4)">pink-4</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--pink-5)">pink-5</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--error-red-light)">error-red-light</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--error-red-mid)">error-red-mid</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--error-red)">error-red</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--success-green-light)">success-green-light</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--success-green-mid)">success-green-mid</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--success-green)">success-green</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--white-shade)">white-shade</ColoredParagrpah>
-        <ColoredParagrpah color="var(--color--black-shade)">black-shade</ColoredParagrpah>
+    <>
+      <Header />
+      <ContentContainer>
+        <GlobalStyle />
 
-      </MainContainer>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="about-us" element={<AboutUs />} />
+          </Routes>
+        </BrowserRouter >
+
+      </ContentContainer>
 
       <Footer />
-    </div>
-
+    </>
   )
 }
+
 export default App
