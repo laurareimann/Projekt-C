64b7863ae81d83c3d177b2b9417e2a0c4ae28e9d
Gandeon <leon-mohr@hotmail.com>
2024-08-14 10:38:52 +0200

    Update App.tsx
    Testseiten-Imports von App.tsx entfernt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,13 +21,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/MainPage";
 import AboutUs from "./pages/AboutUs";
-import LoginTest from './pages/LoginTest.tsx';
-import Signup from './pages/SignUpTest.tsx';
 import { createRoot } from "react-dom/client";
 import Map from "./components/mapComponents/map.tsx";
 import { useLoadScript } from '@react-google-maps/api';
 import StreetProvider from './components/mapComponents/StreetProvider.tsx';
-import SignUpTest from './pages/SignUpTest.tsx';
 import LogInOrSignUp from './pages/LogInPage.tsx';
 import LogInPage from './pages/LogInPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
@@ -185,8 +182,6 @@ const App: React.FC = () => {
         
         <Route index element={<Homepage />} />
         <Route path="about-us" element={<AboutUs />} />
-        <Route path="logintest" element={<LoginTest/>}/>
-        <Route path="signuptest" element={<SignUpTest/>}/>
         <Route path="logInPage" element={<LogInPage/>}/>
         <Route path="registerPage" element = {<RegisterPage/>}/>
         <Route path="profilePage" element ={<ProfilePage/>}/>
